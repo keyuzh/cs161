4a12a5bfd090a7634dd91f6d4ec6b0b2ac50be6d
Keyu Zhang <keyuz4@uci.edu>
2021-04-05 14:37:57 -0700

    update answers

--- PS1/problem4.py
+++ PS1/problem4.py
@@ -16,33 +16,58 @@ NAME = 0
 DIFFICULTY = 1
 HUMOR = 2
 
+# def find_best_prof(A: list, n: int) -> set:
+#     best = set()
+#     # add the professor with the lowest difficulty to the set
+#     best.add(min(A, key=lambda x: x[DIFFICULTY]))
+#     # then add the professor with the highest humor to the set
+#     best.add(max(A, key=lambda x: x[HUMOR]))
+
+#     # then for each professor (P) in array A, 
+#     # if P is not already in set "best", and
+#     # there is no professor in set "best" that is both more funny and 
+#     # less difficult than P
+#     # then add P to set "best"
+#     for p in A:
+#         if p not in best:
+#             # only check for p if not in set
+#             should_add_to_set = True
+#             for i in best:
+#                 if i[DIFFICULTY] < p[DIFFICULTY] and i[HUMOR] > p[HUMOR]:
+#                     # found a professor that is less difficult and more funny
+#                     # do not add to set
+#                     should_add_to_set = False
+#                     break
+#             if should_add_to_set:
+#                 # there is no professor in best that is more funny and less difficult
+#                 # we can add them to set
+#                 best.add(p)
+#     return best
+
 def find_best_prof(A: list, n: int) -> set:
     best = set()
-    # add the professor with the lowest difficulty to the set
-    best.add(min(A, key=lambda x: x[DIFFICULTY]))
-    # then add the professor with the highest humor to the set
-    best.add(max(A, key=lambda x: x[HUMOR]))
+    # sort the array of professors by increasing difficulty
+    # (alternatively, we can also sort by decreasing humor and get the same result)
+    by_difficulty = sorted(A, key=lambda x: x[DIFFICULTY])
 
-    # then for each professor (P) in array A, 
-    # if P is not already in set "best", and
-    # there is no professor in set "best" that is both more funny and 
-    # less difficult than P
-    # then add P to set "best"
-    for p in A:
-        if p not in best:
-            # only check for p if not in set
-            should_add_to_set = True
-            for i in best:
-                if i[DIFFICULTY] < p[DIFFICULTY] and i[HUMOR] > p[HUMOR]:
-                    # found a professor that is less difficult and more funny
-                    # do not add to set
-                    should_add_to_set = False
-                    break
-            if should_add_to_set:
-                # there is no professor in best that is more funny and less difficult
-                # we can add them to set
-                best.add(p)
+    # this variable stores the highest humor score of professors added to the set.
+    # Initiate it as -1 since the range is [0, 100] so that the first (easiest)
+    # professor will be included in set
+    highest_humor = -1
+    for p in by_difficulty:
+        # since the array is sorted by increasing difficulty, the difficulty
+        # of professor p will be greater than every other professor in the set.
+        # therefore, p must have a higher humor score than every professor 
+        # currently in the set to satisfy our selection criteria and be added to set.
+        # Otherwise, p is less funny and more difficult and cannot be added.
+        if p[HUMOR] > highest_humor:
+            # update the highest humor score
+            highest_humor = p[HUMOR]
+            best.add(p)
     return best
+
+
 
 if __name__ == "__main__":
     print(find_best_prof(A, n))
+    print(find_best_2(A, n))
--- PS1/problem5.py
+++ PS1/problem5.py
@@ -1,22 +1,22 @@
 def have_common_key(A: MaxHeap, B: MaxHeap, n: int) -> bool:
-    while n > 0:
-        # get the max element of each max heap and compare them
-        max_A = A.max()
-        max_B = B.max()
-        if max_A == max_B:
-            # common key found
-            return True
-        try:
-            # extract the maximum element of the max heap that has a greater max value
+    try:
+        while n > 0:
+            # get the max element of each max heap and compare them
+            max_A = A.max()
+            max_B = B.max()
+            if max_A == max_B:
+                # common key found
+                return True
+            # extract max from whichever max heap that has larger max value
             if max_A > max_B:
                 A.extractMax()
             else:
                 B.extractMax()
-        except Exception:
-            # assume that extractMax() will throw an exception if the max heap
-            # becomes empty, in this case there is no common key
-            return False
-        # decrement n since we removed an element
-        n -= 1
-    # both max heap becomes empty (n == 0), also indicates no common key
+            # decrement n since we removed an element
+            n -= 1
+    except Exception:
+        # assume that max() and extractMax() will throw an exception if the max heap
+        # becomes empty, in this case there is no common key
+        return False
+    # if both max heap becomes empty (n == 0), also indicates no common key
     return False
